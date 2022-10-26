ee234e70ddf9a7cdc3146734bcff3e52d680e0fc
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-10-26 09:08:02 +0900

    add typescript via cli and set to compile automatically

---
+++ script.ts
@@ -0,0 +1,4 @@
+//in the package.json file, I made this changes:
+//this will compile typescript into JS automatically
+// "start": "tsc --watch"
+// "test": "echo \"Error: no test specified\" && exit 1"
