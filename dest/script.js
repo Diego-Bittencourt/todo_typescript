d55c6981c91b3e8fc475fc13824136a686ad5e9d
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-10-27 12:10:47 +0900

    initial commit

--- dest/script.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-//in the package.json file, I made this changes:
-//this will compile typescript into JS automatically
-// "start": "tsc --watch"
-// "test": "echo \"Error: no test specified\" && exit 1"
-console.log("hello teste");
-
