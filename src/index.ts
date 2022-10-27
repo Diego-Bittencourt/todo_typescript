e972db16b7dec2e1c79c8d03fbd9b0912187872a
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-10-27 12:30:59 +0900

    create handlers to the form, input and button
    I determined the HTML elements using the <> syntax and "as" syntax

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+// import { v4 as uuidV4 } from 'uuid';
 
-import confetti from 'canvas-confetti';
+// console.log(uuidV4());
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+//grab the list, input and button
+const list = document.querySelector<HTMLUListElement>("#list");
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
+
+//there is another way to define the element through the syntax below
+// const form = document.querySelector<HTMLFormElement>("#new-task-form");
+const form = document.getElementById("new-task-form") as HTMLFormElement || null
+
+//the <> element after the function name and before specifies the element
+//defining the element help limit the methods available to the elements.
+
