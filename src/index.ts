23e077fd6e6379fce472ca348d843569f415fd34
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-10-27 13:21:19 +0900

    add checked functionality

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,12 @@ const form =
 //the <> element after the function name and before specifies the element
 //defining the element help limit the methods available to the elements.
 
+//create an array of Tasks to hold the tasks
+const tasks: Task[] = [];
+//the syntax above says the tasks array is an array of Task
+//If you try to push a different data type, it would show an error
+
+
 //I created a custom type to use in the function below
 type Task = {
   id: string;
@@ -39,20 +45,34 @@ form?.addEventListener('submit', (e) => {
   };
 
   addListItem(newTask);
+  input.value = "";
+  tasks.push(newTask);
 });
 
-function addListItem(task: Task) {
+function addListItem(task: Task) : void {
+  //creates an li, label and input element
   const item = document.createElement("li");
   const label = document.createElement("label");
   const checkbox = document.createElement("input");
 
+  //sets the input element to be checkbox type
   checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
 
+  //add the event listener to change status
+  checkbox.addEventListener("change", () => {
+    //assigns the task.completed property to the state in the html element
+    task.completed = checkbox.checked
+  })
+
+  //appends the checkbox and the title after the label
   label.append(checkbox, task.title);
 
+  //appends the label in the li element
   item.append(label);
 
+  //appends the li element in the list
   list?.append(item);
 
-  
+
 }
